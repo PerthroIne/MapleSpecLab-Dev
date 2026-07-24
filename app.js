fc69a2d654f79efb1c4ddaab2621797046e0e4b7
PerthroIne <perthwindj@gmail.com>
2026-07-24 11:54:34 +0900

    Update v1.0-dev.10

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const APP_VERSION = "1.0.0-dev.9";
+const APP_VERSION = "1.0.0-dev.10";
 const BUILD_TIME = "2026-07-24 20:30";
 const STORAGE_KEY = "mapleSpecLabV10Dev5";
 const GITHUB_REPOSITORY = "PerthroIne/MapleSpecLab";
